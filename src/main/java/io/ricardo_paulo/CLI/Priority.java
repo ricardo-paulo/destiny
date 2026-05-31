14c95a8ef1eb09eea865020b696a7b6dc815578c
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2026-05-31 20:17:42 -0300

    Criação do parâmetro priority
    Foi criado o parâmetro obrigatório priority, que o usuário deve informar. O parâmetro pode ser preenchido por: distance, time ou risk.
    
    A depender da prioridade informada, os pesos utilizados nos algoritmos de busca irá mudar.

---
+++ src/main/java/io/ricardo_paulo/CLI/Priority.java
@@ -0,0 +1,7 @@
+package io.ricardo_paulo.CLI;
+
+public enum Priority {
+    distance,
+    time,
+    risk
+}
--- src/main/java/io/ricardo_paulo/CLI/RouteCommand.java
+++ src/main/java/io/ricardo_paulo/CLI/RouteCommand.java
@@ -6,7 +6,7 @@ import picocli.CommandLine.Parameters;
 @Command(
         name = "route",
         mixinStandardHelpOptions = true,
-        version = "route 1.1",
+        version = "route 1.2",
         description = "Traça rotas com base em um grafo de rodovias e cidades."
 )
 public class RouteCommand implements Runnable {
@@ -23,9 +23,16 @@ public class RouteCommand implements Runnable {
     )
     private Algorithm algorithm;
 
+    @Parameters(
+            paramLabel = "<priority>",
+            description = "A prioridade usada para calcular a melhor rota. Disponíveis: ${COMPLETION-CANDIDATES}"
+    )
+    private Priority priority;
+
     @Override
     public void run () {
         System.out.printf("Calculando rota: %s -> %s\n", origin, destiny);
+        System.out.printf("Considerando como prioridade: %s\n", priority);
     }
 
 }
